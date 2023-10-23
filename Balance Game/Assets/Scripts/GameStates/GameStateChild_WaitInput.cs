1c8db19707b1e05681ea25ec0342cdcd52c91a95
TomoNanri <131641585+TomoNanri@users.noreply.github.com>
2023-10-23 16:19:12 +0900

    [update] 施政メニュー選択前のGameOver判定を取りやめ。施政動作完了後に変更。

--- Balance Game/Assets/Scripts/GameStates/GameStateChild_WaitInput.cs
+++ Balance Game/Assets/Scripts/GameStates/GameStateChild_WaitInput.cs
@@ -9,33 +9,20 @@ public class GameStateChild_WaitInput : AbstractStateChild
     private Oshiro _oshiro;
     private bool _isOshiroSelected;
     private GodOfLuck _dice;
-    private bool _isGameOver;
+    //private bool _isGameOver;
     public override void Initialize(int stateType)
     {
         _gm = GetComponent<GameManager>();
         _oshiro = GameObject.Find("Oshiro").GetComponent<Oshiro>();
         _oshiro.OshiroSelected += OnOshiroSelected;
         _dice = FindAnyObjectByType<GodOfLuck>();
-        _isGameOver = false;
 
         base.Initialize(stateType);
     }
     public override void OnEnter()
     {
-        _isGameOver = false;
         Debug.Log($"[{this.name}] Enter WaitInput State!");
-        // 一揆状態ならお殿様の幸運判定を実施する
-        foreach (Mura m in _oshiro.MuraList)
-        {
-            if (m.IsIkkiJoutai)
-            {
-                if (!_dice.DiceCheckD100(_oshiro.Luck, 1))
-                {
-                    _isGameOver = true;
-                }
-            }
-        }
-
+        
         // お城のクリックを可能にし、クリックイベントの受信を開始する
         _oshiro.IsSelectable = true;
         _isOshiroSelected = false;
@@ -51,10 +38,6 @@ public class GameStateChild_WaitInput : AbstractStateChild
     }
     public override int StateUpdate()
     {
-        if (_isGameOver)
-        {
-            return (int)GameManager.StateType.GameOver;
-        }
         if (_isOshiroSelected)
         {
             return (int)GameManager.StateType.InMainMenu;
